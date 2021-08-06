0729a571d320e8f894760f3d252440a4444bacb8
André Anderson Alves Silva <andre.anderson.silva@gmail.com>
2021-08-06 02:10:37 -0300

    SCOR_Rev002

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function regra01(){
 
-    alert("Primiera regra")
+    let res = document.getElementById("flista")
+    res.style.textAlign = "center"
+    res.innerHTML = "<br>Alteração de Regra postada pelo Denilson 26/02/2021 / Orientação para Acionamentos e Escalonamentos de BA 98 com Afetação - UF:SP<br><br>Reforçando:</p><p>1º Acionamento de BA 98 com Afetação</p>>BA 98 com afetação, independentemente da quantidade, precisa ser atendido ou escalonado (Supervisor, Gerente ou Divino)</p><p>Obs: Sempre descrevam dentro do BA o nome das pessoas escalonadas.</p><p>Não temos autorização de segurar acionamentos para horário comercial por qualquer tipo de problema.</p><p>Acionamentos de segunda a segunda.</p><p>Agora é 0 ou 1, ou atende ou escalona.</p><p>⚠️Importante qualquer impedimento no atendimento de campo para 100% dos casos devem ser escalonando o supervisor ou gerente ou Divino. de forma a garantir o pronto atendimento!</p><p>2º Escalonamento de BA 98 com Afetação</p><p>2.1 – Dentro do horário comercial das “07h até as 20h”(Horário comercial: De segunda-feira até sexta-feira, fora  feriados)</p><p>Passados 4h da data de abertura do BA, escalonar Supervisor de campo da região do BA com afetação</p><p>Passados 6h da data de abertura do BA, escalonar Gerente de campo Felix ou Padilha</p><p>Passados 8h da data de abertura do BA, escalonar Divino</p><p>2.1 – Fora do horário comercial das “20:01h até as 06:59h” + sábados, domingos e Feriados</p><p>Passados 4h da data de abertura do BA, escalonar Supervisor de Plantão</p><p>Passados 6h da data de abertura do BA, escalonar Gerente de Plantão</p><p>Passados 8h da data de abertura do BA, escalonar Divino</p><p>Obs: Bas 98 com afetação de TX-LL, não entram na regra (2º Escalonamento) acima.</p>"
+    
+}
+
+function TXLLCOF(){
+    let res = document.getElementById("flista")
+    res.style.textAlign = "center"
+    res.innerHTML = "<p>Alteração de Regra postada pelo Denilson 15/02/2021<<Orientação para tratamento de TX-LL (Reforço) e Avisos de COF com afetação > 13 sites</p> <p>Estamos com o indicador de disponibilidade fora da meta e com isso, peço a gentileza de redobrar a atenção com os dois pontos abaixo:</p><p>1º Avisos de COF com afetação Móvel</p><p>BAs de COF com afetação > 13, avisar imediatamente o Gerente Divino, 24hs x 7 dias da semana.</p><p>2º TX-LL</p><p>Vamos redobrar a atenção com a abertura dos chamados junto com os provedores, principalmente Vivo.</p><p>Para melhorar o tempo de abertura com os provedores, vamos pedir ajuda entre nós, assim entendo que vamos conseguir abrir os chamados mais rapidamente.</p><p>Tenho visto que a solicitação de ajuda entre nós já está acontecendo, isso é muito positivo.</p>" 
 
 }
+
